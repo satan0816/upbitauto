de5bbdde2ced3b7945bbc96d39e593b87be05cf1
satan0816 <87560986+satan0816@users.noreply.github.com>
2021-07-17 18:10:12 +0900

    Add files via upload

--- bitcoinAutoTrade.py
+++ bitcoinAutoTrade.py
@@ -1,5 +1,6 @@
+
 import time
-import pyupbit
+import upbitauto
 import datetime
 
 access = "oF2t3VAMCcjO12ZzW5E5vCXo73QgUdXU1DvpRaJF"
@@ -7,13 +8,13 @@ secret = "OZJS0UHc1dxgM42oCEw7id7gur1j4IwW4wxg4pxL"
 
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
+    df = upbitauto.get_ohlcv(ticker, interval="day", count=2)
     target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
     return target_price
 
 def get_start_time(ticker):
     """시작 시간 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
+    df = upbitauto.get_ohlcv(ticker, interval="day", count=1)
     start_time = df.index[0]
     return start_time
 
@@ -30,10 +31,10 @@ def get_balance(ticker):
 
 def get_current_price(ticker):
     """현재가 조회"""
-    return pyupbit.get_orderbook(tickers=ticker)[0]["orderbook_units"][0]["ask_price"]
+    return upbitauto.get_orderbook(tickers=ticker)[0]["orderbook_units"][0]["ask_price"]
 
 # 로그인
-upbit = pyupbit.Upbit(access, secret)
+upbit = upbitauto.Upbit(access, secret)
 print("autotrade start")
 
 # 자동매매 시작
